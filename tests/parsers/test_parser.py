0696e219b130e4615342811fb7dc3d0aa4202022
AG-SEK <aaron.schueller-ruhl@physik.uni-marburg.de>
2024-10-21 14:06:56 +0200

    Comment out - test_parser.py

--- tests/parsers/test_parser.py
+++ tests/parsers/test_parser.py
@@ -1,13 +1,13 @@
-import logging
-
-from nomad.datamodel import EntryArchive
-
-from nomad_perolab_umr.parsers.parser import CicciTXTParser # Original: NewParser
-
-
-def test_parse_file():
-    parser = NewParser()
-    archive = EntryArchive()
-    parser.parse('tests/data/example.out', archive, logging.getLogger())
-
-    assert archive.workflow2.name == 'test'
+#import logging
+#
+#from nomad.datamodel import EntryArchive
+#
+#from nomad_perolab_umr.parsers.parser import CicciTXTParser # Original: NewParser
+#
+#
+#def test_parse_file():
+#    parser = NewParser()
+#    archive = EntryArchive()
+#    parser.parse('tests/data/example.out', archive, logging.getLogger())
+#
+#    assert archive.workflow2.name == 'test'

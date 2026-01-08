65f92673f5d9dff0ee0f3bbeee06a644fc53c265
Aaron Schüller-Ruhl <aaron.schueller-ruhl@physik.uni-marburg.de>
2026-01-08 10:01:04 +0100

    delete Solar

--- src/nomad_perolab_umr/Solar/plotfunctions/test.py
+++
@@ -1,2 +0,0 @@
-def test_plotfunctions():
-    print("Plofunctions Test worked! Have Fun!")
--- src/nomad_perolab_umr/Solar/plottemplate/__init__.py
+++
@@ -1,5 +0,0 @@
-# This file is required to treat the directory as a Python package.
-# It can also be used to define what should be imported when the package is imported.
-
-# Import module to set UMR template as default
-#import umr_plot_template
